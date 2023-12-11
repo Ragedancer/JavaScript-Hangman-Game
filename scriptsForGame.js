b26bbb8142954e57adb97c6ea69e003ba96128b7
Samuel Groom <Ragedancer00@gmail.com>
2023-12-11 17:34:23 -0500

    Adding body, no styling yet, just text output

--- scriptsForGame.js
+++ scriptsForGame.js
@@ -1,11 +1,13 @@
-let word = 'falcon';
+let word = 'teeth';
 let wordsLines = [];
 let guessLetter;
 let answerLines;
+let wrongGuess;
 
 function confirmValue(){
     guessLetter = document.getElementById('guessLetter').value;
     let i = 0;
+
     console.log('temp word lines, prior to for loop',wordsLines);
     for(const l of word){
         if(guessLetter != l){
@@ -15,12 +17,44 @@ function confirmValue(){
            wordsLines[i] = guessLetter;
             i++;
         }
-
-
     }
+    if(!word.includes(guessLetter)){
+        console.log("Hitting first one",wrongGuess);
+            wrongGuess++;
+        }
+    displayHangman();
     let tmpwrd = displayGuessWord();
     answerLines.innerHTML = tmpwrd;
 
+}
+
+function displayHangman(){
+    let body = document.getElementById('bodyOutput');
+    let gameOutput;
+    switch (wrongGuess) {
+        case 1:
+          gameOutput = "head";
+          break;
+          case 2:
+          gameOutput = "head,torso";
+          break;
+          case 3:
+          gameOutput = "head,torso,arm1";
+          break;
+          case 4:
+          gameOutput = "head,torso,arm1,arm2";
+          break;
+          case 5:
+          gameOutput = "head,torso,arm1,arm2,leg1";
+          break;
+          case 6:
+          gameOutput = "head,toso,arm1,arm2,leg1,leg2";
+          console.log("You lose");
+          break;
+        default:
+          gameOutput = "aint no body yet";
+      }
+body.innerHTML = gameOutput;
 }
 
 function displayGuessWord(){
@@ -35,6 +69,7 @@ function displayGuessWord(){
 }
 
 function generateLinesForWord(){
+    wrongGuess = 0;
     answerLines = document.getElementById('answerInLines');
     for(const l of word){
         wordsLines.push('_');
