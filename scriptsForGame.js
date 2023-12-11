b21a9ad9b65835b67e560140b34ae17163252480
Samuel Groom <Ragedancer00@gmail.com>
2023-12-11 14:41:34 -0500

    Corrected function to correctly find and replace empty line with letter guessed

--- scriptsForGame.js
+++ scriptsForGame.js
@@ -5,55 +5,42 @@ let answerLines;
 
 function confirmValue(){
     guessLetter = document.getElementById('guessLetter').value;
-    //console.log(document.getElementById('answerInLines').textContent);
     let i = 0;
-    let tempWordLines =  wordsLines;
-    console.log('temp word lines, prior to for loop',tempWordLines);
+    console.log('temp word lines, prior to for loop',wordsLines);
     for(const l of word){
         if(guessLetter != l){
             i++;
-           // console.log('If statement',guessLetter,word,i,tempWordLines);
         }
         else if(guessLetter == l){
-           tempWordLines = [wordsLines.slice(0,i),guessLetter,wordsLines.slice(i)];
+           wordsLines[i] = guessLetter;
             i++;
-           // console.log('Else if statement',guessLetter,word,i,tempWordLines);
         }
 
+
     }
-    answerLines.innerHTML = tempWordLines;
-    resetWordLines();
-    //wordsLines = tempWordLines;
+    let tmpwrd = displayGuessWord();
+    answerLines.innerHTML = tmpwrd;
 
 }
 
-function resetWordLines(){
-    console.log('Log wordLines before for loop',wordsLines);
-    let num = wordsLines.length;
-    for(let i = 0; i < num; i++){
-        console.log('Log wordLines during for loop',wordsLines);
-        wordsLines.pop();
-    }
-    console.log(document.getElementById('answerInLines').textContent);
-    //wordsLines = document.getElementById('answerInLines').textContent; 
-    console.log('Log wordLines after for loop',wordsLines);
-        wordLines = [];
-        for(let l of document.getElementById('answerInLines').textContent){
-            if(l != ','){
-                wordsLines.push(l);
-            }
-        }
-        console.log('Log wordLines after 2nd for loop',wordsLines);
-        
-    }
+function displayGuessWord(){
+    let tempWord = '';
+    wordsLines.forEach(l =>{
+        tempWord += l + ' ';
+    });
+    console.log(tempWord);
+    return tempWord;
 
+    
+}
 
 function generateLinesForWord(){
     answerLines = document.getElementById('answerInLines');
     for(const l of word){
         wordsLines.push('_');
     }
-    answerLines.innerHTML = wordsLines;
+    let genwrd = displayGuessWord();
+    answerLines.innerHTML = genwrd;
     console.log('Generate lines for words', answerLines);
 }
 
